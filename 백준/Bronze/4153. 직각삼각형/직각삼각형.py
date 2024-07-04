1dbd5e81ca815b5f1558ba2e69fa50d00ea349df
SonYoungJun <63299624+young-dune@users.noreply.github.com>
2024-07-04 19:11:46 +0900

    Update 직각삼각형.py

--- 백준/Bronze/4153. 직각삼각형/직각삼각형.py
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.py
@@ -2,7 +2,7 @@ while (1):
     a,b,c = map(int, input().split())
     if a==0 and b==0 and c==0:
         break
-    
+    #항상 c가 빗변이라는 보장이 없으니 가장 큰 숫자를 c로 보내서 직각삼각형을 판단한다.
     if max(a,b,c) == a:
         a,c = c,a
     elif max(a,b,c) == b:
